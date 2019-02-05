c712c077912c2ff67a96a477867b0d948ee6e009
JamieRez <jamierezendes@gmail.com>
2019-02-05 15:15:19 -0800

    Now you can see if you're at make school

--- server.go
+++ server.go
@@ -5,11 +5,16 @@ import (
 	"github.com/labstack/echo"
   "encoding/json"
   "io/ioutil"
+  "fmt"
 )
 
 
 type Song struct {
   Lyrics string `json:"lyrics"`
+}
+
+type IP struct {
+  Ip string `json:"ip"`
 }
 
 func getSongLyrics(artist string, title string) Song {
@@ -30,9 +35,37 @@ func getSongLyrics(artist string, title string) Song {
   return newSong
 }
 
+func getExternalIP() IP {
+  resp, err := http.Get("https://api.ipify.org?format=json")
+  if err != nil {
+    panic(err)
+  }
+  defer resp.Body.Close()
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    panic(err)
+  }
+
+  extIp := IP{}
+  json.Unmarshal(body, &extIp)
+  return extIp
+}
+
+func getReqLocation(next echo.HandlerFunc) echo.HandlerFunc {
+  return func(c echo.Context) error {
+    ip := getExternalIP()
+    fmt.Println("You are at Make School:")
+    fmt.Println(ip.Ip == "96.78.162.189")
+    c.Response().Header().Set(echo.HeaderServer, "Echo/3.0")
+    return next(c)
+  }
+}
+
 
 func main() {
 	e := echo.New()
+  e.Use(getReqLocation)
 	e.GET("/:artist/:song", func(c echo.Context) error {
     newSong := getSongLyrics(c.Param("artist"), c.Param("song"));
 		return c.String(http.StatusOK, newSong.Lyrics)
